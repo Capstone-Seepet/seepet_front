3f417a9e5a1d272ac64999d3c3f0719783040f5a
박총명 <qkrri56@naver.com>
2024-04-17 14:29:00 +0900

    [ENH] header styling

--- src/commons/compononets/header/HeaderLayout.js
+++ src/commons/compononets/header/HeaderLayout.js
@@ -1,10 +1,20 @@
 import './HeaderLayout.css'
+import {Link} from "react-router-dom";
 const HeaderLayout = () => {
   return (
     <>
       <header>
-        <div className="header-left"></div>
-        <div className="header-right"></div>
+        <div className="header-wrap">
+          <div className="header-left">
+            <Link to="/">
+              <img src="images/logo.svg" alt="Home" className="logo"/>
+            </Link>
+          </div>
+          <div className="header-right">
+            <img src="images/icon_bell.svg" alt="alarm"/>
+            <img src="images/icon_people.svg" alt="mypage"/>
+          </div>
+        </div>
       </header>
     </>
   )
