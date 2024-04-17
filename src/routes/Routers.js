342061e824e58ae03484ff04a08cf0135e7c680c
박총명 <qkrri56@naver.com>
2024-04-17 13:52:21 +0900

    [ENH] Router 설정

---
+++ src/routes/Routers.js
@@ -0,0 +1,16 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import MainPage from "../views/main/MainPage";
+import LoginPage from "../views/login/LoginPage";
+import RegisterPage from "../views/register/RegisterPage";
+
+const Routers = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LoginPage />}></Route>
+      <Route path="/main" element={<MainPage />}></Route>
+      <Route path="/register" element={<RegisterPage />}></Route>
+    </Routes>
+  </BrowserRouter>
+)
+
+export default Routers
---
+++ src/views/login/LoginPage.js
@@ -0,0 +1,12 @@
+import {Link} from "react-router-dom";
+
+const LoginPage = () => {
+  return (
+    <>
+      <h1>Login</h1>
+      <Link to="/register">register</Link>
+    </>
+  )
+}
+
+export default LoginPage
