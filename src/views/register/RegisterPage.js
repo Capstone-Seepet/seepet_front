860a48c8695311690ac6e72dce56d461ef690fcd
박총명 <qkrri56@naver.com>
2024-04-21 21:33:36 +0900

    [ENH] 수정

--- src/views/register/RegisterPage.js
+++ src/views/register/RegisterPage.js
@@ -1,10 +1,14 @@
 import HeaderLayout from "../../commons/compononets/header/HeaderLayout";
+import FooterLayout from "../../commons/compononets/footer/FooterLayout";
 
 const RegisterPage = () => {
   return (
     <>
       <HeaderLayout />
-      <h1>회원가입</h1>
+      <main>
+        <h1>회원가입</h1>
+      </main>
+      <FooterLayout />
     </>
   )
 }
