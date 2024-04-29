b6cde17148df26198ec1f04ff71c8de91700fe5d
박총명 <qkrri56@naver.com>
2024-04-29 16:05:40 +0900

    [ENH] 강아지 정보 등록 스타일 적용

--- src/views/dog-register/DogRegisterPage.js
+++ src/views/dog-register/DogRegisterPage.js
@@ -10,47 +10,88 @@ const DogRegisterPage = () => {
           <h1 className={style.title}>반려동물 정보 입력</h1>
           <div className={style.container}>
             <form action="">
-              <div className={style.inputWrap}>
-                <label htmlFor="">이미지 등록</label>
-                <div className={style.inputBox}>
-                  <input type="file" name=""/>
-                </div>
+              <div className={style.thumbnailWrap}>
+                <label htmlFor="thumbnail">
+                  <img src={process.env.PUBLIC_URL + "/images/icon_plus.svg"} alt="plus"/>
+                  <img src={process.env.PUBLIC_URL + "/images/icon_thumbnail.svg"} alt="이미지 등록"/>
+                  <div className={style.labelDeco}>
+                    <img src={process.env.PUBLIC_URL + "/images/icon_camera.svg"} alt="camera"/>
+                  </div>
+                </label>
+                <input type="file" id="thumbnail" name="thumbnail" />
               </div>
               <div className={style.inputWrap}>
                 <label htmlFor="name">이름</label>
-                <input type="text" name="name"/>
+                <input type="text" name="name" className={style.longInput}/>
               </div>
               <div className={style.inputWrap}>
                 <label htmlFor="">이미지 등록</label>
                 <div className={style.inputBox}>
-                  <input type="file" name="image"/>
-                  <input type="file" name="image"/>
-                  <input type="file" name="image"/>
-                  <input type="file" name="image"/>
+                  <div className={style.fileWrap}>
+                    <label htmlFor="image">
+                      <img src={process.env.PUBLIC_URL + "/images/icon_plus.svg"} alt="plus"/>
+                      <img src={process.env.PUBLIC_URL + "/images/icon_thumbnail.svg"} alt="이미지 등록"/>
+                    </label>
+                    <input type="file" id="image" className={style.inputFile}/>
+                  </div>
+                  <div className={style.fileWrap}>
+                    <label htmlFor="image2">
+                      <img src={process.env.PUBLIC_URL + "/images/icon_plus.svg"} alt="plus"/>
+                      <img src={process.env.PUBLIC_URL + "/images/icon_thumbnail.svg"} alt="이미지 등록"/>
+                    </label>
+                    <input type="file" id="image2" className={style.inputFile}/>
+                  </div>
+                  <div className={style.fileWrap}>
+                    <label htmlFor="image3">
+                      <img src={process.env.PUBLIC_URL + "/images/icon_plus.svg"} alt="plus"/>
+                      <img src={process.env.PUBLIC_URL + "/images/icon_thumbnail.svg"} alt="이미지 등록"/>
+                    </label>
+                    <input type="file" id="image3" className={style.inputFile}/>
+                  </div>
+                  <div className={style.fileWrap}>
+                    <label htmlFor="image4">
+                      <img src={process.env.PUBLIC_URL + "/images/icon_plus.svg"} alt="plus"/>
+                      <img src={process.env.PUBLIC_URL + "/images/icon_thumbnail.svg"} alt="이미지 등록"/>
+                    </label>
+                    <input type="file" id="image4" className={style.inputFile}/>
+                  </div>
                 </div>
               </div>
               <div className={style.inputWrap}>
                 <label htmlFor="">성별</label>
                 <div className={style.inputBox}>
-                  <label htmlFor="">수컷</label>
-                  <input type="radio"/>
-                  <label htmlFor="">암컷</label>
-                  <input type="radio"/>
+                  <div className={style.radioWrap}>
+                    <label htmlFor="">
+                      수컷
+                      <img src={process.env.PUBLIC_URL + "/images/icon_gender_male.svg"} alt="male"/>
+                    </label>
+                    <input type="radio"/>
+                  </div>
+                  <div className={style.radioWrap}>
+                    <label htmlFor="">
+                      암컷
+                      <img src={process.env.PUBLIC_URL + "/images/icon_gender_female.svg"} alt="male"/>
+                    </label>
+                    <input type="radio"/>
+                  </div>
                 </div>
               </div>
               <div className={style.inputWrap}>
                 <label htmlFor="">우리가 처음 만난 날</label>
-                <input type="text"/>
+                <input type="date" className={style.longInput}/>
               </div>
               <div className={style.inputBox}>
-                <div className={style.inputWrap}>
+                <div className={`${style.inputWrap} ${style.doubleWrap}`}>
                   <label htmlFor="">생년월일</label>
-                  <input type="text"/>
+                  <input type="date" className={style.longInput}/>
                 </div>
-                <div className={style.inputWrap}>
+                <div className={`${style.inputWrap} ${style.doubleWrap}`}>
                   <label htmlFor="">몸무게</label>
-                  <input type="text"/>
+                  <input type="text" className={style.longInput}/>
                 </div>
+              </div>
+              <div className={style.btnBox}>
+                <input type="submit" className={style.submitBtn} value="등록하기"/>
               </div>
             </form>
           </div>
