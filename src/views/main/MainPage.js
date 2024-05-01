2cbe23093353e4daded43b0046128ae288b2a2d4
박총명 <qkrri56@naver.com>
2024-05-01 20:11:35 +0900

    [ENH] 메인 슬라이드 적용

--- src/views/main/MainPage.js
+++ src/views/main/MainPage.js
@@ -1,11 +1,25 @@
 import HeaderLayout from "../../commons/compononets/header/HeaderLayout";
 import FooterLayout from "../../commons/compononets/footer/FooterLayout";
 import style from "./MainPage.module.css"
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./slide.css"
 
 const MainPage = () => {
   const today = new Date();
   const formattedYear = today.getFullYear().toString().slice(-2);
   const formattedFull = `${formattedYear}년 ${today.getMonth() + 1}월 ${today.getDate()}일`;
+  var settings = {
+    className: "center",
+    centerMode: true,
+    infinite: false,
+    centerPadding: "60px",
+    slidesToShow: 1,
+    speed: 500,
+    dots: true,
+  };
+
   return (
     <>
       <HeaderLayout />
@@ -48,6 +62,19 @@ const MainPage = () => {
         </div>
         <div className={style.dogToday}>
           <p>재롱이의 {formattedFull}</p>
+          <div className={style.slideWrap}>
+          <Slider {...settings}>
+            <div className={`${style.todayCard} ${style.todayDiary}`}>
+
+            </div>
+            <div className={`${style.todayCard} ${style.todayStatics}`}>
+
+            </div>
+            <div className={`${style.todayCard} ${style.todayObserve}`}>
+
+            </div>
+          </Slider>
+          </div>
         </div>
       </main>
       <FooterLayout/>
