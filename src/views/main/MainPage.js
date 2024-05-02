a00bf3714f91dd17ca541d6fd84fcf168e917996
박총명 <qkrri56@naver.com>
2024-05-02 17:26:20 +0900

    [ENH] chart 적용

--- src/views/main/MainPage.js
+++ src/views/main/MainPage.js
@@ -5,6 +5,37 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./slide.css"
 import {Link} from "react-router-dom";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export const data = {
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
 
 const MainPage = () => {
   const today = new Date();
@@ -132,7 +163,7 @@ const MainPage = () => {
                       </div>
                     </div>
                     <div className={style.cardMain}>
-                      <circle strokeDasharray="<a> <x - a>" strokeDashoffset="<0.25 * x>"/>
+                      <Doughnut  data={data}/>
                     </div>
                   </div>
                 </div>
