88d4674aa8ef950d3183b51c113450de78199b63
AhnRian0 <00rian@naver.com>
2024-05-02 18:15:31 +0900

    add: 시작페이지, 로그인 완료 페이지, 실시간 영상 수신 테스트

--- src/views/startPage/StartPage.js
+++ src/views/startPage/StartPage.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import SharedHeader from "../../commons/compononets/SharedHeader/SharedHeader";
 import style from "./StartPate.module.css";
 const StartPage = () => {
@@ -6,12 +8,14 @@ const StartPage = () => {
       <div className={style.wrap}>
         <SharedHeader isStartPage={true} />
         <div className={style.bottomWrap}>
-          <div className={style.startWrap}>
-            <button className={style.startBtn}>
-              <div className={style.pawimg} />
-            </button>
-            <p className={style.startText}>시작하기</p>
-          </div>
+          <Link to="/login">
+            <div className={style.startWrap}>
+              <button className={style.startBtn}>
+                <div className={style.pawimg} />
+              </button>
+              <p className={style.startText}>시작하기</p>
+            </div>
+          </Link>
           <div className={style.bottomImg}></div>
         </div>
       </div>
