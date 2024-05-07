9f43101f21629aa7a030e32bea16cbe1c6a2505b
박총명 <qkrri56@naver.com>
2024-05-07 13:24:14 +0900

    [ENH] 페이지 추가

---
+++ src/views/dogDiary/DogDiaryPage.js
@@ -0,0 +1,11 @@
+import style from "./DogDiaryPage.modle.css"
+import ExpendedHeader from "../../commons/compononets/ExpendedHeader/ExpendedHeader";
+const DogDiaryPage = () => {
+  return (
+    <>
+      <ExpendedHeader />
+    </>
+  )
+}
+
+export default DogDiaryPage;
