bb20bff3dd10b015ef54fd0a3f3444083b636872
AhnRian0 <00rian@naver.com>
2024-05-09 18:18:21 +0900

    add: 환경설정 페이지 디자인(1차)

--- src/routes/Routers.js
+++ src/routes/Routers.js
@@ -8,18 +8,31 @@ import MainPage from "../views/main/MainPage";
 import DogDiaryPage from "../views/dogDiary/DogDiaryPage";
 import DogStatisticsPage from "../views/dogStatistics/DogStatisticsPage";
 import LiveStream from "../views/liveStream/liveStream";
+import SetupPage from "../views/setupPage/SetupPage";
 const Routers = () => (
   <BrowserRouter>
     <Routes>
+      {/* 메인 */}
       <Route path="/" element={<StartPage />}></Route>
+      <Route path="/main" element={<MainPage />}></Route>
+
+      {/* 로그인, 회원가입 */}
       <Route path="/register" element={<RegisterPage />}></Route>
       <Route path="/login" element={<LoginPage />}></Route>
       <Route path="/loginsuccess" element={<LoginSuccess />}></Route>
+
+      {/* 강아지등록 */}
       <Route path="/dog/register" element={<DogRegisterPage />}></Route>
-      <Route path="/main" element={<MainPage />}></Route>
+
+      {/* 일지 및 통계 */}
       <Route path="/dog/diary" element={<DogDiaryPage />}></Route>
       <Route path="/dog/statistics" element={<DogStatisticsPage />}></Route>
+
+      {/* 실시간 영상 */}
       <Route path="/livestream" element={<LiveStream />}></Route>
+
+      {/* 환경설정 */}
+      <Route path="/setup" element={<SetupPage />}></Route>
     </Routes>
   </BrowserRouter>
 );
