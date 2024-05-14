fc93302c96730bbb47618164adae5b10b700d960
박총명 <qkrri56@naver.com>
2024-05-14 15:25:17 +0900

    [ENH] 강아지 통계 달력 디자인
    강아지 통계 달력 디자인
    readme 업데이트

--- src/views/dogStatistics/DogStatisticsPage.js
+++ src/views/dogStatistics/DogStatisticsPage.js
@@ -1,9 +1,21 @@
 import style from './DogStatisticsPage.module.css'
 import ExpendedHeader from "../../commons/compononets/ExpendedHeader/ExpendedHeader";
+import Calendar from "react-calendar";
+import {useState} from "react";
+import "./calendar.css"
 const DogStatisticsPage = () => {
+  const [value, onChange] = useState(new Date());
+
   return (
     <>
       <ExpendedHeader />
+      <main>
+        <div className={style.calendarWrap}>
+          <Calendar onChange={onChange} value={value}
+                    className="statisticsCalendar"
+                    maxDetail="month"/>
+        </div>
+      </main>
     </>
   )
 }
